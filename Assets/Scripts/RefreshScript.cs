16604711f232e8ca8a6b3d18ba02109b34b765bd
wesselvink <22092@ma-web.nl>
2018-10-31 14:53:40 +0100

    ProcedurallyDungeon

---
+++ Assets/Scripts/RefreshScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RefreshScript : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown ("r")) {
+			SceneManager.LoadScene ("game");
+		}
+	}
+}
